8a777fee4fb9b3ef70c12b4c2dbb3fda1c3b4a76
MattWoelk <matt.woelk@gmail.com>
2019-04-22 20:34:47 -0500

    commit #2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use specs::{World, Component, VecStorage, System, SystemData, ReadStorage};
 use specs::world::Builder;
+use specs::{Component, ReadStorage, System, SystemData, VecStorage, World};
 use tcod::colors;
 use tcod::console::*;
 //use proc_macro::Delimiter;
@@ -18,6 +18,14 @@ impl Component for Position {
     type Storage = VecStorage<Self>;
 }
 
+struct CharacterGlyph {
+    glyph: char,
+}
+
+impl Component for CharacterGlyph {
+    type Storage = VecStorage<Self>;
+}
+
 #[derive(Debug, Default)]
 struct PrintMeTag;
 
@@ -25,8 +33,13 @@ impl Component for PrintMeTag {
     type Storage = specs::NullStorage<Self>;
 }
 
+#[derive(Debug, Default)]
+struct GameState {
+    end: bool,
+}
+
 struct PrintingSystem;
-impl <'a> System<'a> for PrintingSystem {
+impl<'a> System<'a> for PrintingSystem {
     type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, PrintMeTag>);
 
     fn run(&mut self, data: Self::SystemData) {
@@ -40,7 +53,7 @@ impl <'a> System<'a> for PrintingSystem {
 }
 
 struct NotPrintingSystem;
-impl <'a> System<'a> for NotPrintingSystem {
+impl<'a> System<'a> for NotPrintingSystem {
     type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, PrintMeTag>);
 
     fn run(&mut self, data: Self::SystemData) {
@@ -50,6 +63,35 @@ impl <'a> System<'a> for NotPrintingSystem {
         for (pos, _) in (&position, !&print_me).join() {
             println!("{:?}", pos);
         }
+    }
+}
+
+struct Render {
+    window: Root,
+}
+
+impl<'a> System<'a> for Render {
+    type SystemData = (
+        ReadStorage<'a, CharacterGlyph>,
+        ReadStorage<'a, Position>,
+        specs::Write<'a, GameState>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        use specs::Join;
+
+        let root = &mut self.window;
+
+        let (sprites, positions, mut game_state) = data;
+
+        root.clear();
+        for (sprite, pos) in (&sprites, &positions).join() {
+            root.put_char(pos.x, pos.y, sprite.glyph, BackgroundFlag::None);
+        }
+        root.flush();
+        root.wait_for_keypress(false);
+
+        game_state.end = root.window_closed();
     }
 }
 
@@ -66,38 +108,40 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
     let mut world = World::new();
+    world.add_resource(GameState { end: false });
 
     let mut dispatcher = specs::DispatcherBuilder::new()
         .with(PrintingSystem, "print_sys", &[])
         .with(NotPrintingSystem, "not_print_sys", &["print_sys"])
+        .with_thread_local(Render { window: root })
         .build();
 
     dispatcher.setup(&mut world.res);
 
-    world.create_entity()
-        .with(Position {x: 10, y: 10})
+    world
+        .create_entity()
+        .with(Position { x: 10, y: 10 })
         .with(PrintMeTag {})
+        .with(CharacterGlyph { glyph: 'y' })
         .build();
 
-    world.create_entity()
-        .with(Position {x: 20, y: 10})
+    world
+        .create_entity()
+        .with(Position { x: 20, y: 10 })
+        .with(CharacterGlyph { glyph: 'o' })
         .build();
 
-    world.create_entity()
-        .with(Position {x: 30, y: 10})
+    world
+        .create_entity()
+        .with(Position { x: 30, y: 10 })
+        .with(CharacterGlyph { glyph: 'T' })
         .build();
 
-    dispatcher.dispatch(&mut world.res);
-
-    let mut x = 10;
-    let mut y = 10;
-
-    while !root.window_closed() {
-        root.clear();
-        root.put_char(x, y, '@', BackgroundFlag::None);
-        root.flush();
-        root.wait_for_keypress(false);
-        x += 1;
-        y += 1;
+    loop {
+        dispatcher.dispatch(&mut world.res);
+        let game_state = world.read_resource::<GameState>();
+        if game_state.end {
+            break;
+        }
     }
 }
